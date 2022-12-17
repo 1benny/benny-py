b412c994fce09144db78bc062e70c01bd648012c
bdxves <bdives@icloud.com>
2022-12-17 21:29:34 +1100

    ~~

---
+++ temp.py
@@ -0,0 +1,9 @@
+import requests as r
+
+def send(phone, message):
+       
+#   phone = "+61414939515"
+#   message = input(">> ")
+    key = "c5f912d774345c869eeff6ac49d8f992af352742mmaF09ybB8CDZbW6dLuSCqzKd"
+    
+    return
