32c0daae6d20fa2c8fe245ee807f3360e099a580
bdxves <bdives@icloud.com>
2022-12-18 14:30:03 +1100

    ~~

--- temp.py
+++ temp.py
@@ -1,9 +1,10 @@
 import requests as r
+import customtkinter as ttkk
 
-def send(phone, message):
-       
-#   phone = "+61414939515"
-#   message = input(">> ")
-    key = "c5f912d774345c869eeff6ac49d8f992af352742mmaF09ybB8CDZbW6dLuSCqzKd"
+class SMS(object):
+    def __init__(self, phone, message):
+        self.phone = phone
+        self.message = message
     
-    return
+    def send(self):
+        return
