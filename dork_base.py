d469d37c59331530baf6189b5f82a82a6de020c3
bdxves <bdives@icloud.com>
2023-01-13 02:31:37 +1100

    Created base class files for dork-style search tool

---
+++ dork_base.py
@@ -0,0 +1,12 @@
+import requests
+import os
+import sys
+import subprocess
+
+class dork:
+    def __init__(self, query, format):
+        self.query = query
+        self.format = format
+
+    def search():
+        dork_get = 
