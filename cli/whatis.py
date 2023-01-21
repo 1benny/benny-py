5313285e3a5205f9043f614a435863b074b7e335
bdxves <bdives@icloud.com>
2023-01-22 06:54:46 +1100

    ~

--- cli/whatis.py
+++ cli/whatis.py
@@ -2,19 +2,30 @@ import argparse
 import time
 import subprocess as sub
 
+def descript(page):
+    with open(page, "r", encoding="utf-8") as f:
+        desc = f.readlines()[3]
+        print(f"\n{desc[6:-1]}")
+        f.close()
+    return
+
+def missing_page(page):
+    with open(page, "a") as f:
+        f.write(f"{args.a}\n")
+        f.close()
+        print(f"Appended '{args.a}' to man-page download helper")
+    return
+
 cli = argparse.ArgumentParser(prog="whatis for windows", description="general what is that program")
 cli.add_argument("a", type=str, help="Required positional argument for query")
 args = cli.parse_args()
 
 database = "C:/Users/Ben/Man/"
 search = ((f"{database}{args.a}.txt").replace("/", "\\"))
-manpage_installer = "C:\\Users\\Ben\\Man\\python\\missing\\"
+missing_dir = "C:\\Users\\Ben\\Man\\scripts\\missing.txt"
 
 try:
-    with open(search, "r", encoding="utf-8") as f:
-        desc = f.readlines()[3]
-        print(f"\n{desc[6:-1]}")
-        f.close()
+    descript(page=search)
 except FileNotFoundError:
     try:
         time.sleep(2)
@@ -22,11 +33,10 @@ except FileNotFoundError:
         time.sleep(1)
         missing = input("Make an entry for missing manpage? y/n: ")
         if missing == "y" or missing == "Y":
-            with open(manpage_installer, "a") as f:
-                f.write(f"{args.a}\n")
-                f.close()
-                print(f"\nAppended '{args.a}' to man-page download helper")
-        else:
+            missing_page(page=missing_dir)
+        elif missing == "n" or missing == "N":
             print("\nNo worries.")
+        elif KeyboardInterrupt:
+            print(f"\nWhatever fuckwit")
     except KeyboardInterrupt:
         print(f"\nWhatever fuckwit")
