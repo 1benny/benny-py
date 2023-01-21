60886302d63e0e024111a641c48cea1e9e4304a7
bdxves <bdives@icloud.com>
2023-01-22 06:52:44 +1100

    Edited lots Changed lots

--- cli/ls.py
+++ cli/ls.py
@@ -1,2 +1,35 @@
-import os 
-os.system("dir")
+import os
+import argparse as arg
+from argparse import *
+
+def list_dir():
+    for i in os.listdir(pwd):
+        print(i)
+
+
+
+
+def long(pwd):
+    contents = []
+    for i in os.listdir(pwd):
+        if os.path.exists(i in range(len(i)) == True):
+            print(i)
+#       print(f"{i}    ~>   {}")
+#       contents.append(i)
+#       for name in contents:
+#           each = os.path.join(pwd, name)
+#           print(each)
+
+
+
+
+
+ls = ArgumentParser(prog="ls for windows", description="Linux/Unix-style listing program except its for windows because i created it and we all love windows")
+ls.add_argument("dir", type=str, default=False, help="Required positional argument for name of directory to list")
+ls.add_argument("-l", metavar="--long", default=False, help="Present list in longer format with extra details")
+args = ls.parse_args()
+
+pwd = os.getcwd()
+
+if args.l:
+    long(pwd=pwd)
--- cli/whatis.py
+++ cli/whatis.py
@@ -1,39 +1,32 @@
-import os
 import argparse
 import time
+import subprocess as sub
 
 cli = argparse.ArgumentParser(prog="whatis for windows", description="general what is that program")
 cli.add_argument("a", type=str, help="Required positional argument for query")
 args = cli.parse_args()
 
+database = "C:/Users/Ben/Man/"
+search = ((f"{database}{args.a}.txt").replace("/", "\\"))
+manpage_installer = "C:\\Users\\Ben\\Man\\python\\missing\\"
 
-database = "C:/Users/Ben/whatis/"
-search = f"{database}{args.a}.txt"
-search = search.replace("/", "\\")
-
-if os.path.exists(search):
-    try:
-        with open(search.replace("/", "\\"), "r", encoding="utf-8") as f:
-            answer = f.read()
-            print(answer)
-            f.close
-    except UnicodeDecodeError:
-        print("Nothing appropriate")
-    except UnicodeError:
-        print("Nothing appropriate")
-else:
+try:
+    with open(search, "r", encoding="utf-8") as f:
+        desc = f.readlines()[3]
+        print(f"\n{desc[6:-1]}")
+        f.close()
+except FileNotFoundError:
     try:
         time.sleep(2)
-        print("Something went wrong... Nothing appropriate")
+        print("Nothing found in man-pages")
         time.sleep(1)
         missing = input("Make an entry for missing manpage? y/n: ")
         if missing == "y" or missing == "Y":
-            with open("C:\\Users\\Ben\\Desktop\\shit\\missing-manpages.txt", "a") as f:
+            with open(manpage_installer, "a") as f:
                 f.write(f"{args.a}\n")
-                print(f"\nAdded entry for missing '{args.a}' description")
                 f.close()
+                print(f"\nAppended '{args.a}' to man-page download helper")
         else:
             print("\nNo worries.")
-            
     except KeyboardInterrupt:
-        print("\nWhatever fuckwit")
+        print(f"\nWhatever fuckwit")
